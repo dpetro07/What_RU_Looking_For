35b69311ca53b2665413c986827d2bf57d1c21db
Johall <johnalexander.w.hall@gmail.com>
2016-02-24 18:51:40 -0500

    added server code for heroku database

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('mysql://b558e03c477434:44e2e75f@us-cdbr-iron-east-03.cleardb.net/');
+
+app.use(require('express-session')({
+ secret: 'magnusrex',
+ cookie: { secure: false,
+  maxAge: 1000 * 60 * 60 * 24 * 14
+ },
+ saveUninitialized: true,
+ resave: true
+}));
+
+var session = require('express-session');
+var bcrypt = require('bcryptjs');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
+var expressHandlebars = require('express-handlebars');
+app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+sequelize.sync().then(function() {
+ app.listen(PORT, function() {
+  console.log("LISTENING!");
+ });
+});
