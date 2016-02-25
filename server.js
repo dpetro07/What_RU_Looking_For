aa934cf066d59158fffa1b07b20a2bd336abec80
Johall <johnalexander.w.hall@gmail.com>
2016-02-24 21:11:45 -0500

    added a basic input for a user via sequelize

--- server.js
+++ server.js
@@ -28,6 +28,47 @@ var expressHandlebars = require('express-handlebars');
 app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+var User = sequelize.define('User', {
+ email: {
+  type: Sequelize.STRING,
+  validate: {
+   len: {
+    args: [5,30],
+   },
+   isEmail: true
+  },
+  unique: true,
+  allowNull: false
+ },
+ password: {
+  type: Sequelize.STRING,
+  validate: {
+   len: {
+    args: [5,12],
+    msg: "Your password must contain 5-12 characters"
+   }
+  },
+   allowNull: false
+ },
+ username: {
+  type: Sequelize.STRING,
+  validate: {
+   len: {
+    args: [1,30],
+    msg: "You must have a user name"
+   }
+  },
+  allowNull: false
+ }
+}, {
+ hooks: {
+  beforeCreate: function(input){
+   input.password = bcrypt.hashSync(input.password, 10);
+  }
+ }
+});
+
+
 app.get('/', function(req, res){
   res.render('home');
 });
@@ -38,11 +79,29 @@ app.get('/addreview', function(req, res){
 
 app.get('/showreviews', function(req, res){
   res.render('listing');
-})
+});
 
 app.get('login', function(req, res){
   res.render('login');
-})
+});
+
+app.post('/register', function(req, res){
+  User.create(req.body).then(function(user){
+    res.redirect('listing');
+  }).catch(function(err){
+    console.log(err);
+    res.redirect('/?msg=' + err.message);
+  });
+});
+
+app.post('/newreview', function(req, res){
+  Review.create(req.body).then(function(user){
+    res.redirect('home');
+  }).catch(function(err){
+    console.log(err);
+    res.redirect('/?msg=' + err.message);
+  });
+});
 
 sequelize.sync().then(function() {
  app.listen(PORT, function() {
