91cfeea92fe18e8eb21d2270aa73f3451d25e55d
WolfgangHall <johnalexander.w.hall@gmail.com>
2016-02-27 10:32:44 -0500

    added remaining jquery form validation checks

--- models/js/validate.js
+++ models/js/validate.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
+  
   $("#loginform").validate({
     rules: {
     username: {
+      required: true,
+      minlength: 2
+    },
+    firstname: {
       required: true,
       minlength: 2
     },
@@ -16,8 +21,58 @@ $(document).ready(function(){
     }
   },
     messages: {
-      name: "Required Field"
+      "username": {
+        required: "You must enter a username",
+        minlength: "You must use at least 2 characters"
+      },      
+      "firstname": {
+        required: "You must enter a first name",
+        minlength: "You must use at least 2 characters"
+      },      
+      "password": {
+        required: "You must enter a password",
+        minlength: "You must use at least 7 characters"
+      },
+      "email": {
+        required: "You must enter a email",
+        minlength: "You must use at least 6 characters",
+        email: "You must enter a valid email"
+      }
     }
   });
 
+
+  $("#reviewform").validate({
+    rules: {
+      category: {
+        required: true
+      },
+      address: {
+        required: true
+      },
+      price: {
+        required: true
+      },
+      description: {
+        required: true,
+        minlength: 2,
+        maxlength: 500
+      }
+    },
+    messages: {
+      "category": {
+        required: "You must select a category"
+      },      
+      "address": {
+        required: "You must enter an address"
+      },      
+      "price": {
+        required: "You must select a price"
+      },
+      "description": {
+        required: "You must enter a description",
+        minlength: "You must use at least 2 characters",
+        maxlength: "You cannot use more than 500 characters"
+      }
+    }
 });
