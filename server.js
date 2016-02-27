11fc99fe5ef897bc6a44e22bc3487f7257d90dfc
RM <rm@nbp-150-107.nbp.ruw.rutgers.edu>
2016-02-27 11:12:55 -0500

    added registration and login forms to login.handlebars,set-up setup packages and added user authentication code to server.js

---
+++ server.js
@@ -0,0 +1,126 @@
+//express setup
+var express = require('express');
+var app = express();
+var port = 3000;
+
+//database setup
+var Sequelize = require('sequelize');
+var connection = new Sequelize('user_authentication_db', 'root');
+
+//requiring passport last
+var passport = require('passport');
+var passportLocal = require('passport-local');
+//middleware init
+app.use(require('express-session')({
+    secret: 'automagic',
+    resave: true,
+    saveUninitialized: true,
+    cookie : { secure : false, maxAge : (4 * 60 * 60 * 1000) }, // 4 hours
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//passport use methed as callback when being authenticated
+passport.use(new passportLocal.Strategy(function(username, password, done) {
+    //check password in db
+    User.findOne({
+        where: {
+            username: username
+        }
+    }).then(function(user) {
+        //check password against hash
+        if(user){
+            bcrypt.compare(password, user.dataValues.password, function(err, user) {
+                if (user) {
+                	//if password is correct authenticate the user with cookie
+                  done(null, { id: username, username: username });
+                } else{
+                	done(null, null);
+                }
+            });
+        } else {
+            done(null, null);
+        }
+    });
+
+}));
+
+//change the object used to authenticate to a smaller token, and protects the server from attacks
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+    done(null, { id: id, username: id })
+});
+
+var bcrypt = require("bcryptjs");
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+var User = connection.define('user', {
+	username: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		unique: true
+	},
+	password: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			len: {
+				args: [5,10],
+				msg: "Your password must be between 5-10 characters"
+			},
+			isUppercase: true
+		}
+	}
+}, {
+	hooks: {
+		beforeCreate: function(input){
+			input.password = bcrypt.hashSync(input.password, 10);
+		}
+	}
+});
+
+
+//handlebars setup
+var expressHandlebars = require('express-handlebars');
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+//check login with db
+app.post('/check', passport.authenticate('local', {
+    successRedirect: '/listing',
+    failureRedirect: '/?msg=Login Credentials do not work'
+}));
+
+
+app.get("/", function(req, res){
+	res.render('login', {msg: req.query.msg});
+})
+
+app.get('/login', function(req, res){
+	res.render('login', {
+    user: req.user,
+    isAuthenticated: req.isAuthenticated()
+  });
+})
+app.post("/save", function(req, res){
+	User.create(req.body).then(function(result){
+		res.redirect('/?msg=Account created');
+	}).catch(function(err) {
+		console.log(err);
+		res.redirect('/?msg=' + err.errors[0].message);
+	});
+})
+
+// database connection via sequelize
+connection.sync().then(function() {
+  app.listen(port, function() {
+      console.log("Listening on:" + port)
+  });
+});
